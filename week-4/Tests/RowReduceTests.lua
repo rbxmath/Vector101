391c3966d25364758426c46207691d7f0b6d1e3c
ecurtiss <ethanhcurtiss@gmail.com>
2023-06-22 00:34:35 -0700

    Add row reduce tests

--- week-4/Tests/RowReduceTests.lua
+++ week-4/Tests/RowReduceTests.lua
@@ -1,6 +1,22 @@
 local Matrix = require("Matrix")
 local RowReduce = require("RowReduce")
 
+local function matrixToMathematica(matrix)
+	local strings = table.create(matrix.rows + 2)
+	strings[1] = "{"
+
+	for i, row in ipairs(matrix) do
+		strings[i + 1] = "{" .. table.concat(row, ", ") .. "},\n"
+	end
+
+	strings[matrix.rows + 1] = strings[matrix.rows + 1]:sub(1, -3)
+	strings[matrix.rows + 2] = "}"
+
+	return table.concat(strings)
+end
+
+function Matrix:ToTableString() end
+
 local long = Matrix.new({
 	{ 4, 2, 4, 6, 7, 2 },
 	{ 2, 1, 3, 7, 9, 1 },
@@ -25,4 +41,16 @@ local invertible = Matrix.new({
 	{ 0, 0, 0, 2, 2 },
 })
 
-print(RowReduce(long))
+local random = {}
+for i = 1, 10 do
+	local row = {}
+
+	for j = 1, 20 do
+		row[j] = math.random()
+	end
+
+	random[i] = row
+end
+random = Matrix.new(random)
+
+print(RowReduce(random))
